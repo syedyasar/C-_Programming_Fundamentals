265823df63ab353a2a22d23686284dc84150d8c7
syedyasar <syedyasar.mjcet@gmail.com>
2021-06-13 17:40:33 -0700

    10_cout_string

---
+++ 01_Introduction/10_cout_string.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using  namespace   std;
+main()
+{
+	string  s = "Rama Rao";
+	cout << s << "\n"; 	//  Rama  Rao
+}
+
+
+
+/*
+1)  operator  >>  function  reads  input  and  operator <<  writes  output
+
+2) operator >> function reads  input  until  space bar , tab or enter key
+
+3) operator  <<  function writes string  until  '\0'
+*/
+
+
