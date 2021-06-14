9261b13e6970506e6a6b171550dc7248bfe9847e
syedyasar <syedyasar.mjcet@gmail.com>
2021-06-13 17:39:40 -0700

    04_cin_with_multi_word_string

---
+++ 01_Introduction/04_cin_with_multi_word_string.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using  namespace   std;
+main()
+{
+	string   s;
+	cout << "Enter  any  string  :  ";
+	cin >> s; // Syed  Yasar
+	cout<<"s : " << s << "\n"; // Syed 
+}
+
+
+/*
+1)  If  input  is  Syed  Yasar ,  operator  >>   function  reads  only Syed
+
+2) In  general,  operator  >>   function  reads  single  word  string  but  not  multi-word  string
+
+3) It  reads  string  until  user  strikes  space bar , tab  key  (or) enter  key
+
+4)  There  is  string  datatype  in   c++
+*/
+
