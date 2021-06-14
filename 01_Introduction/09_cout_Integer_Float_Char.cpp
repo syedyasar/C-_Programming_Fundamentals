24edc1bfe069e03d2b4e0aeb4b0f1e64a2243cc8
syedyasar <syedyasar.mjcet@gmail.com>
2021-06-13 17:40:26 -0700

    09_cout_Integer_Float_Char

---
+++ 01_Introduction/09_cout_Integer_Float_Char.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using  namespace   std;
+main()
+{
+	int   x = 25;
+	float y = 10.8;
+	char  ch = 'g';
+	cout << x << "\n" << y << "\n" << ch << "\n";
+	cout << x << "\t" << y << "\t" << ch << "\n";
+	cout << x << "," << y << "," << ch << "\n";
+}
+
+
