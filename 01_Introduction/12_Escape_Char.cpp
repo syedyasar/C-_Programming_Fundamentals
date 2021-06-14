107d5e57f283ad82e88911607e3d4b3d3a5b2727
syedyasar <syedyasar.mjcet@gmail.com>
2021-06-13 17:40:48 -0700

    12_Escape_Char

---
+++ 01_Introduction/12_Escape_Char.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using  namespace   std;
+main()
+{
+	cout << "\\" << "\n";  //  
+	cout << "\"" << "\n"; // "
+	cout << "\\n" << "\n"; // \n
+	cout << "\"  C++ \"   is   a   great  language \n"; // "C++"  is   a  great  language
+}
+
+
+/*
+1) To  print  '\',  another '\'  is  needed
+
+2) To  print  " ,  \  is  needed 
+
+3) To print \n , another \  is  needed
+*/
+
+
