798dfb9d229b50ef24562ba319f36c975d092d16
syedyasar <syedyasar.mjcet@gmail.com>
2021-06-13 17:39:53 -0700

    05_Getline

---
+++ 01_Introduction/05_Getline.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using  namespace   std;
+main()
+{
+	string  s;
+	cout << "Enter  any  string  :  ";
+	getline(cin , s); // Syed Yasar
+	cout<<"s : " << s << "\n"; // Syed  Yasar
+}
+
+/*
+1) getline  function  can  read  multiword string
+
+2) It  is  defined in <iostream>   file
+
+3) It  has  2  parameters
+    a)  cin
+	b)  string variable (may be 's')
+
+4) operator  >>  function  is  used  to  read  integer , float  and  character  but  not  string
+*/
+
