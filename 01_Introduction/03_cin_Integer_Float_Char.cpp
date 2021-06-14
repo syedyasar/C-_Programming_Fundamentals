e26e96e75947c14752e368ea2401f7fa343ec099
syedyasar <syedyasar.mjcet@gmail.com>
2021-06-13 17:39:28 -0700

    03_cin_Integer_Float_Char

---
+++ 01_Introduction/03_cin_Integer_Float_Char.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using  namespace   std;
+main()
+{
+	int   x;
+	float  y;
+	char  ch;
+	cout << "Enter  integer number , float  number  and  character \n";
+	cin >> x >> y >> ch; //  25   10.8   g
+	cout<<"x : " << x << "\n"; // 25
+	cout<<"y : " << y << "\n"; // 10.8 
+	cout<<"ch : " << ch << "\n"; // g
+}
+
+
