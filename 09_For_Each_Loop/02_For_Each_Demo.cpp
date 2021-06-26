707a7bc94178af34f5be4442b7714bdd4e6f5316
syedyasar <syedyasar.mjcet@gmail.com>
2021-06-25 22:29:23 -0700

    02_For_Each_Demo

---
+++ 09_For_Each_Loop/02_For_Each_Demo.cpp
@@ -0,0 +1,11 @@
+// for .. each  loop  demo  program
+#include <iostream>
+using  namespace std;
+main()
+{
+	float  a[] = {10.8 , 20.6 , 15.2 , 18.3};
+	for(float  x :  a)  // x  is  each  element  of  array  'a'
+			cout << x << "\n";  // 10.8  20.6  15.2   18.3
+}
+
+// Online COmpiler: https://replit.com/languages/cpp?v2=1
