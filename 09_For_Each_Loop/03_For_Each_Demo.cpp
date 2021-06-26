8c48fc6bce91e2720630010df4c17128b8781e10
syedyasar <syedyasar.mjcet@gmail.com>
2021-06-25 22:29:31 -0700

    03_For_Each_Demo

---
+++ 09_For_Each_Loop/03_For_Each_Demo.cpp
@@ -0,0 +1,9 @@
+// for .. each  loop  demo  program
+#include <iostream>
+using  namespace std;
+main()
+{
+	char  a[] = "Vamsi";
+	for(char   ch  :  a)  // ch  is  each  character   of  array  'a'
+			cout << ch << "\n";  // V     a     m     s     i
+}
