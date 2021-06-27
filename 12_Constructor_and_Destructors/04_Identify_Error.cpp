44b7cf1f2e0dc0bd3d600cee9c43708e5acb1d65
syedyasar <syedyasar.mjcet@gmail.com>
2021-06-26 23:11:18 -0700

    04_Identify_Error

---
+++ 12_Constructor_and_Destructors/04_Identify_Error.cpp
@@ -0,0 +1,14 @@
+//  Identify  error
+#include <iostream>
+using  namespace  std;
+class  Rat
+{
+	public:
+		Rat()
+		{
+			return  25; //  Error  becoz   return  is  not  allowed
+		}
+};
+main()
+{
+}
