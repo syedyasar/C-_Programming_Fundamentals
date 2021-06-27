1b6da314245319aff4d4fb00d5f81483a2cab2fb
syedyasar <syedyasar.mjcet@gmail.com>
2021-06-26 23:11:11 -0700

    03_Identify_Error

---
+++ 12_Constructor_and_Destructors/03_Identify_Error.cpp
@@ -0,0 +1,14 @@
+//  Identify  error
+#include <iostream>
+using  namespace  std;
+class  Rat
+{
+	public:
+		void   Rat()  //  Error  becoz   returnn  type  is not  allowed
+		{
+			cout << "Constructor\n";
+		}
+};
+main()
+{
+}
