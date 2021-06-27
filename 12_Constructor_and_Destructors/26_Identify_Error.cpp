2e3400f567acdc4b5c3929e5513a11fca17d75c6
syedyasar <syedyasar.mjcet@gmail.com>
2021-06-26 23:14:08 -0700

    26_Identify_Error

---
+++ 12_Constructor_and_Destructors/26_Identify_Error.cpp
@@ -0,0 +1,13 @@
+// Identify  error
+#include <iostream>
+using   namespace  std;
+class  Test
+{
+	public:
+		~Test(int  x)  // Error  becoz   destructor  can not  have  parameter
+		{
+		}
+};
+main()
+{
+}
