2502503401c722c848483a95af71ec9908a3b464
syedyasar <syedyasar.mjcet@gmail.com>
2021-06-28 19:07:48 -0700

    11_Identify_Error

---
+++ 16_Virtual_Function/11_Identify_Error.cpp
@@ -0,0 +1,14 @@
+// Identify  error
+#include <iostream>
+using  namespace  std;
+class  c1
+{
+	public:
+		virtual  void   f1() = 0  //   Error  becoz  PVF  can  not  have  body
+		{
+		}
+};
+main()
+{
+}
+
