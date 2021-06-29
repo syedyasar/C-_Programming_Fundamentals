846942f90173683bf4e77f345839787aceac5653
syedyasar <syedyasar.mjcet@gmail.com>
2021-06-29 11:05:19 -0700

    02_Identify_Error

---
+++ 17_Operator_Overloading/02_Identify_Error.cpp
@@ -0,0 +1,10 @@
+//  Identify  error
+#include  <iostream>
+using  namespace  std;
+int&   f1()  // It  is   RBR   function
+{
+	return  25; //  Error  becoz  value  can  not  be   returned
+}
+main()
+{
+}
